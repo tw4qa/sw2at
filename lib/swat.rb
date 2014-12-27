4977dbacf36206592f03462cb28186d0f7737f42
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2014-12-27 18:17:27 +0200

    allow to enter empty opts

--- lib/swat.rb
+++ lib/swat.rb
@@ -1,6 +1,10 @@
 module Swat
 
-  def self.setup(rspec_config, opts)
+  DEFAULT_OPTIONS = {
+    capybara: {}
+  }
+
+  def self.setup(rspec_config, opts = DEFAULT_OPTIONS)
     Swat::Capybara.setup rspec_config, opts[:capybara]
   end
 
