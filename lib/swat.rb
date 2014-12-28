83a3764dc4e000414d4ed9ac39181cc57f4f4305
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2014-12-28 14:46:13 +0200

    add TW config

--- lib/swat.rb
+++ lib/swat.rb
@@ -3,11 +3,13 @@ module Swat
   require 'swat_tw'
 
   DEFAULT_OPTIONS = {
-    capybara: {}
+    capybara: {},
+    test_world: {},
   }
 
   def self.setup(rspec_config, opts = DEFAULT_OPTIONS)
     Swat::Capybara.setup rspec_config, opts[:capybara]
+    Swat::TestWorld.setup rspec_config, opts[:test_world]
   end
 
 end
